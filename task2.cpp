c25e67c7868858aecef3bfd89d2491c58039f383
r9jgfja <112956409+DenisShestakov1@users.noreply.github.com>
2022-12-06 10:27:38 +0700

    Update task2.cpp

--- task2.cpp
+++ task2.cpp
@@ -2,26 +2,26 @@
 #define N 3
 int main() {
      
-    int Counter = 1;
+    int case = 1;
     int j = 1;
     int i = 0;
     int array[N][N];
 
     for (i; i < N; i++) {
-        array[0][i] = Counter;
-        Counter++;
+        array[0][i] = case;
+        case++;
     }
     for (j; j < N; j++) {
-        array[j][i - 1] = Counter;
-        Counter++;
+        array[j][i - 1] = case;
+        case++;
     }
     for (int i = N - 2; i >= 0; i--) {
-        array[j - 1][i] = Counter;
-        Counter++;
+        array[j - 1][i] = case;
+        case++;
     }
     for (int i = 0; i <= N - 2; i++) {
-        array[1][i] = Counter;
-        Counter++;
+        array[1][i] = case;
+        case++;
     }
     for (int i = 0; i < N; i++) {
         for (int j = 0; j < N; j++) {
